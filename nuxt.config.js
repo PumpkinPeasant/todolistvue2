e982bc22a9609626d91de08dbdb5c926335d3e65
PumpkinPeasant <m2314670@edu.misis.ru>
2025-05-06 15:17:04 +0300

    Changed project Vue version to Vue2

---
+++ nuxt.config.js
@@ -0,0 +1,19 @@
+export default {
+  mode: 'universal',
+
+  head: {
+    title: 'TODO List Vue 2',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Описание проекта' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  css: [],
+  plugins: [],
+  modules: [],
+  build: {}
+}
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script>
+
+</script>
+
+<template>
+  <div>
+    Hello world!
+  </div>
+</template>
+
+<style scoped>
+
+</style>
